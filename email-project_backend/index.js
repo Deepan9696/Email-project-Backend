70fbcaa7759b05e93abf38a80a6640523d519130
DEEPANRAJ <118879053+Deepan9696@users.noreply.github.com>
2023-11-16 11:57:33 +0530

    Add files via upload

---
+++ email-project_backend/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import Connection from './database/db.js';
+import routes from './routes/route.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded());
+app.use(express.json());
+app.use('/', routes);
+
+app.get("/", function(req, res){
+    res.send({message:"Welcome to Email App"})
+  })
+
+  
+const PORT = process.env.PORT;
+
+Connection();
+
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
